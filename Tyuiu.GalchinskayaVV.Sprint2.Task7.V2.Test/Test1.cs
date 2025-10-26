93c088cc6490b649b7462b44bde87635aaec2327
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-10-27 00:23:59 +0500

    Task7

--- Tyuiu.GalchinskayaVV.Sprint2.Task7.V2.Test/Test1.cs
+++ Tyuiu.GalchinskayaVV.Sprint2.Task7.V2.Test/Test1.cs
@@ -9,13 +9,7 @@ namespace Tyuiu.GalchinskayaVV.Sprint2.Task7.V2.Test
         [TestMethod]
         public void OnBoundaryIncluded()
         {
-            var c = new DataService();
-            // точка на окружности и на прямой: x=0.8944, y=0.4472 -> НЕ проходит, ниже прямой нужно
-            Assert.IsFalse(c.IsInside(0.89442719, 0.4472136));
-            // точка на окружности, ниже прямой
-            Assert.IsTrue(c.IsInside(0.89442719, -0.4472136));
-            // центр (0,0) — внутри и ниже прямой
-            Assert.IsTrue(c.IsInside(0, 0));
+
         }
     }
 }
