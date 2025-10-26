85c2b6b21ee7b1e2c8001720829218b91b4d106f
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-10-26 22:58:33 +0500

    Task5

--- Tyuiu.GalchinskayaVV.Sprint2.Task5.V2.Lib/Class1.cs
+++ Tyuiu.GalchinskayaVV.Sprint2.Task5.V2.Lib/Class1.cs
@@ -1,46 +1,30 @@
 ﻿using System;
 using tyuiu.cources.programming.interfaces.Sprint2;
-using static System.Net.Mime.MediaTypeNames;
+using tyuiu.cources.programming.interfaces.Sprint2;
 
 namespace Tyuiu.GalchinskayaVV.Sprint2.Task5.V2.Lib
 {
     public class Class1 : ISprint2Task5V2
     {
-        private int month;
-
         public string FindMonthSeason(int value)
         {
-            string season;
-
-            switch (month)
+            switch (value)
             {
                 case 12:
                 case 1:
-                case 2:
-                    season = "Зима";
-                    break;
+                case 2: return "зима";
                 case 3:
                 case 4:
-                case 5:
-                    season = "Весна";
-                    break;
+                case 5: return "весна";
                 case 6:
                 case 7:
-                case 8:
-                    season = "Лето";
-                    break;
+                case 8: return "лето";
                 case 9:
                 case 10:
-                case 11:
-                    season = "Осень";
-                    break;
-                default:
-                    season = "Ошибка: номер месяца должен быть от 1 до 12";
-                    break;
+                case 11: return "осень";
+                default: return "ошибка";
             }
-            return season;
         }
-
     }
-
 }
+
